be67b547746137354f1ea9d168e9e225629ed241
Yega Cho <yega1009@gmail.com>
2023-09-21 19:57:16 -0400

    imported modules and initialized express and used middleware

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const uuid = require('./helpers/uuid');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
