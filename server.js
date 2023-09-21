ed01e6f0b384d02ed5752a51570c1499d4a7fd07
Yega Cho <yega1009@gmail.com>
2023-09-21 19:58:23 -0400

    created get route to read db.json file and return data as json

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: "An error occurred reading the database." });
+        }
+        return res.json(JSON.parse(data));
+    });
+});
