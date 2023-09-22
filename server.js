312adbd1142976b453cc39b424b1ed7ee1d77e0a
Yega Cho <yega1009@gmail.com>
2023-09-21 20:00:21 -0400

    created route to serve notes.html file, default route to serve index.html, and start express server

--- server.js
+++ server.js
@@ -41,3 +41,15 @@ app.post('/api/notes', (req, res) => {
         });
     });
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
