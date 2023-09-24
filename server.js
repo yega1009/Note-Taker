b82395e6036d1ad2d2f4be0cd56628ff652bc44a
Yega Cho <yega1009@gmail.com>
2023-09-24 19:43:48 -0400

    simplified server.js file for modularization

--- server.js
+++ server.js
@@ -1,54 +1,57 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const html = require('.routes/html');
+const api = require('.routes/api');
 
-const uuid = require('./helpers/uuid');
+// const path = require('path');
+// const fs = require('fs');
+// const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
-app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
-        if (err) {
-            return res.status(500).json({ error: "An error occurred reading the database." });
-        }
-        return res.json(JSON.parse(data));
-    });
-});
+app.use(html);
+app.use(api);
 
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuid();
+// app.get("/api/notes", (req, res) => {
+//     fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+//         if (err) {
+//             return res.status(500).json({ error: "An error occurred reading the database." });
+//         }
+//         return res.json(JSON.parse(data));
+//     });
+// });
 
-    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
-        if (err) {
-            return res.status(500).json({ error: "An error occurred reading the database. "});
-        }
-        const notes = JSON.parse(data);
-        notes.push(newNote);
+// app.post('/api/notes', (req, res) => {
+//     const newNote = req.body;
+//     newNote.id = uuid();
 
-        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(notes), (err) => {
-            if (err) {
-                return res.status(500).json({ error: "An error occurred writing to the database." });
-            }
-            return res.json(newNote);
-        });
-    });
-});
+//     fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+//         if (err) {
+//             return res.status(500).json({ error: "An error occurred reading the database. "});
+//         }
+//         const notes = JSON.parse(data);
+//         notes.push(newNote);
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
+//         fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(notes), (err) => {
+//             if (err) {
+//                 return res.status(500).json({ error: "An error occurred writing to the database." });
+//             }
+//             return res.json(newNote);
+//         });
+//     });
+// });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+// app.get('/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, "./public/notes.html"));
+// });
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
