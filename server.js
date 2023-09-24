6b1133d4419069b507f20e1071e9e73230f8a68d
Yega Cho <yega1009@gmail.com>
2023-09-24 19:50:12 -0400

    deleted commented out sections

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const html = require('.routes/html');
 const api = require('.routes/api');
 
-// const path = require('path');
-// const fs = require('fs');
-// const uuid = require('./helpers/uuid');
-
 const PORT = 3001;
 const app = express();
 
@@ -15,14 +11,6 @@ app.use(express.static('public'));
 
 app.use(html);
 app.use(api);
-
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, "./public/notes.html"));
-// });
-
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./public/index.html"));
-// });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
