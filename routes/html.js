8ab22c28e9aeec1e8e459c9933e01b4df41fb8b1
Yega Cho <yega1009@gmail.com>
2023-09-24 19:49:52 -0400

    updated html.js file to include GET functions

---
+++ routes/html.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+module.exports = router;
